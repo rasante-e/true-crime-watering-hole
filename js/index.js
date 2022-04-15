4f99944ded933eff9ac1c1fa943830f20968c7c0
Rashida A <rashida@raeccleston.com>
2022-04-15 10:43:24 -0400

    Added .env file

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 const https = require('https');
+var watchify = require('watchify');
+var browserify = require('browserify');
+
+require('dotenv').config();
+console.log(process.env);
 
 //Code to fetch IMDb list data and convert to an HTML list
 var movieList = document.getElementById("actual-imdb-list");
@@ -23,6 +28,60 @@ async function buildList () {
 }
 
 buildList();
+
+
+var postList = document.getElementById("actual-reddit-list");
+
+
+
+
+
+
+
+//Fetch for Reddit posts
+  /*
+fetch("https://www.reddit.com/r/TellMeAFact/top/.json?count=20").then(function (response) {
+    return response.json();
+    //return response;
+}).then (function(obj) {
+
+    for (var i = 0; i < obj.length; i++) {
+    console.log(i);
+    }
+  
+    for (var i = 0; i < obj.length; i++) {
+        var movie = document.createElement("li");
+        movieList.appendChild(movie);
+        movie.classList.add("retrieved");
+        movie.innerHTML = "Name: " + obj[i].app_name + "<br>" + "Type: " + obj[i].app_author + "<br>" + "URL: " + obj[i].app_semantic_version;
+        
+    }
+
+    
+}).catch (function(error) {
+    console.error("There's an error");
+    console.error(error);
+});
+*/
+
+
+
+/*
+const redditFetch = require('reddit-fetch');
+
+redditFetch({
+
+    subreddit: 'UnresolvedMysteries',
+    sort: 'new',
+    allowNSFW: false,
+    allowModPost: true,
+    allowCrossPost: true,
+    allowVideo: true
+
+}).then(post => {
+    console.table(post);
+});
+*/
 
 
 //Connecting to RestDB, which contains user's wishlist
@@ -59,3 +118,4 @@ https.get(options, (res) => {
 }).on("error", (error) => {
     console.error(error.message);
 });
+
